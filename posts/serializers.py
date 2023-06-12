be124b0577a211a13b0e56077c24a4390b46d1a4
tturgum5ekov <tturgum5ekov@gmail.com>
2023-06-12 19:27:10 +0600

    third commit

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from category.models import Category
 from .models import Post
 
 
@@ -9,7 +11,9 @@ class PostListSerializer(serializers.ModelSerializer):
 
 
 class PostCreateSerializer(serializers.ModelSerializer):
-    category = serializers.IntegerField(required=True)
+    category = serializers.PrimaryKeyRelatedField(required=True, queryset=Category.objects.all())
+    owner = serializers.ReadOnlyField(source='owner.id')
+
     class Meta:
         model = Post
         fields = '__all__'
